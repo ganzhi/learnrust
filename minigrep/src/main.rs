bf531b4e60aa468a6271e67783bcd6214c0460b6
James Gan <ganzhi@gmail.com>
2020-09-07 21:00:10 -0700

    Refactor things into lib.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,25 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
+use minigrep::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("With text:\n{}", contents);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
+
+
