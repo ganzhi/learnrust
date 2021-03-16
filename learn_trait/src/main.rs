60792685fac91555cf28bcd17fd6a82cfef866b5
james <ganzhi@gmail.com>
2021-03-15 22:11:54 -0700

    Added my first poorman's iterator (it requires clone)

--- learn_trait/src/main.rs
+++ learn_trait/src/main.rs
@@ -1,3 +1,50 @@
+trait MyIter {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+struct MyIterator<'a, T:Clone> {
+    index:usize,
+    vv: &'a Vec<T>
+}
+
+impl<'a, T:Clone> MyIterator<'a, T> {
+    fn new(v: &'a Vec<T>) -> Self {
+        MyIterator{index:0, vv:v}
+    }
+}
+
+impl<'a, T:Clone> MyIter for MyIterator<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T>
+    {
+        let ind = self.index;
+        if ind < self.vv.len() {
+            self.index = ind + 1;
+            return Some(self.vv[ind].clone());
+        }
+        return None;
+    }
+}
+
+struct MyVec<T> {
+    v: Vec<T>,
+}
+
+impl<T:Clone> MyVec<T> {
+    fn new() -> Self {        
+        MyVec{
+            v: Vec::new()
+        }
+    }
+
+    fn iter<'a>(self: &'a Self) -> MyIterator<'a, T>{
+        MyIterator::new(&self.v)
+    }
+}
+
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T
 {
     let mut largest = &list[0];
@@ -21,4 +68,14 @@ fn main() {
 
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+
+    let mut myvect = MyVec::new();
+    for i in 1..10 {
+        myvect.v.push(i);
+    }
+
+    let mut iter = myvect.iter();
+    while let Some(v) = iter.next() {
+        println!("{}", v);
+    }
 }
