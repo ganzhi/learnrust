2cd52e1b125f4a8e6ee63e08cdea0d2ffb6a4542
james <ganzhi@gmail.com>
2021-04-18 17:08:00 -0700

    minor modification to reduce warnings

--- asyncb/src/unsafet.rs
+++ asyncb/src/unsafet.rs
@@ -1,18 +1,15 @@
-use std::mem::size_of;
-
-use futures_util::future::join;
-
-static mut hello:i32 = 32;
+static mut HELLO:i32 = 32;
 
 fn main() {
     pretty_env_logger::init();
 
-    let mut s = [1, 2, 3, 4, 5];
+    let s = [1, 2, 3, 4, 5];
     let p = &s as *const i32;
 
-    unsafe {
+    let s = unsafe {
         println!("{}", *p.offset(1));
-        hello = 48;
-    }
-    println!("The global hello is {}", hello);
+        HELLO = 48;
+        HELLO
+    };
+    println!("The global HELLO is {}", s);
 }
