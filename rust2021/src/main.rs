468defe7ca5df794a104fcbd4dae3fefdcb21633
James Gan <ganzhi@gmil.com>
2021-10-31 16:56:52 -0700

    Try another 2021 change

--- rust2021/src/main.rs
+++ rust2021/src/main.rs
@@ -3,4 +3,16 @@ fn main() {
         println!("{}", e);
     }
     println!("Hello, world!");
+
+    // Now closure only capture what it reads.
+    let x = 10;
+    let c = || {
+        let _ = x; // no-op and no data is read. Thus x can be used later
+    };
+    c();
+
+    let c = || match x {
+        _ => println!("Hello World from closure!")
+    };
+    c();
 }
