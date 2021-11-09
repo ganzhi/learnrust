fce62c3495ee0181de290f7f7d6a5fc904b53902
james <ganzhi@gmail.com>
2021-11-09 09:02:29 -0800

    Try to create a data structure

--- datastruct/src/main.rs
+++ datastruct/src/main.rs
@@ -1,18 +1,18 @@
-struct Node<'a> {
+struct Node {
     value: i32,
-    left: Option<&'a Node<'a>>,
-    right: Option<&'a Node<'a>>
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>
 }
 
 fn main() {
     let child1 = Node{value:2, left:None, right: None};
     let child2 = Node{value:1, left:None, right: None};
-    let parent = Node {value:0, left:Some(&child1), right:Some(&child2)};
+    let parent = Node {value:0, left:Some(Box::new(child1)), right:Some(Box::new(child2))};
     print!("The value of parent is {}\n", parent.value);
     if let Some(c) = parent.left {
         print!("The value of left child is {}\n", c.value);
     }
     if let Some(c) = parent.right {
         print!("The value of right child is {} \n", c.value);
-    }    
+    }
 }
