54fe4ecdabdb75dc8f457de36332cef16883f463
james <ganzhi@gmail.com>
2021-11-10 08:07:14 -0800

    Adding atomic integer

--- datastruct/src/main.rs
+++ datastruct/src/main.rs
@@ -1,3 +1,11 @@
+use std::ops::Add;
+use std::slice::SliceIndex;
+use std::thread;
+use std::time::Duration;
+
+use std::sync::Arc;
+use std::sync::atomic::{AtomicI64, AtomicUsize, Ordering};
+
 struct Node {
     value: i32,
     left: Option<Box<Node>>,
@@ -15,4 +23,36 @@ fn main() {
     if let Some(c) = parent.right {
         print!("The value of right child is {} \n", c.value);
     }
+
+    let handle = thread::spawn(move || {
+        for i in 1..10 {
+            print!("The value of parent is {} \n", parent.value);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    handle.join().unwrap();
+
+    let count = Arc::new(AtomicI64::new(0));
+    let count_c = count.clone();
+    let thread1 = thread::spawn(move|| {
+        for i in 0..10000 {
+            count_c.as_ref().fetch_add(1, Ordering::SeqCst);
+        }
+    });
+    let count_c = count.clone();
+    let thread2 = thread::spawn(move|| {
+        for i in 0..10000 {
+            count_c.as_ref().fetch_add(1, Ordering::SeqCst);
+        }
+    });
+    let count_c = count.clone();
+    let thread3 = thread::spawn(move|| {
+        for i in 0..10000 {
+            count_c.as_ref().fetch_add(1, Ordering::SeqCst);
+        }
+    });
+    thread1.join();
+    thread2.join();
+    thread3.join();
+    println!("The final count is {:?} \n", count);
 }
