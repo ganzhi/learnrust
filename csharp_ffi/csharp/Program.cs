d9c7365952e9b97abc96367e3afcc158738b1188
james <ganzhi@gmail.com>
2022-12-04 14:06:42 -0800

    Add a small program to call rust from C#

---
+++ csharp_ffi/csharp/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.InteropServices;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! {0}", add(3, 5));
+
+[DllImport("../target/debug/csharp_ffi.dll")]
+static extern long add(long a, long b);
